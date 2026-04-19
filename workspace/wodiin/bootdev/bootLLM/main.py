bdd6f6c7779f4737948eb44ffd22f25081d9a7fc
Wodiin <samphillips246@gmail.com>
2026-04-20 01:03:19 +1000

    comments in main

--- workspace/wodiin/bootdev/bootLLM/main.py
+++ workspace/wodiin/bootdev/bootLLM/main.py
@@ -8,14 +8,20 @@ api_key = os.environ.get("GEMINI_API_KEY")
 if api_key is None:
     raise RuntimeError("GEMINI_API_KEY is not set in the environment variables.")
 
+# Initialize the Gemini API client
 client = genai.Client(api_key=api_key)
 
+# Generate content using the Gemini API
 response = client.models.generate_content(model="gemini-2.5-flash", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+
+# Check if the response contains usage metadata and print token counts
 if response.usage_metadata != None:
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 else:
     raise RuntimeError("Failed API Request") 
+
+# Print the generated content
 print(response.text)
 
 
